00ddfb2ab4cf529569992a1a4ccfb6905b64ab9d
TieNan2019 <cooldownlengque@163.com>
2020-11-01 15:16:18 +0800

    {MultiMedia : add : qt projection; }

---
+++ MultiMedia/main.cpp
@@ -0,0 +1,13 @@
+#include "player.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+        QApplication a(argc, argv);
+        Player w;
+
+
+        w.show();
+        return a.exec();
+}
